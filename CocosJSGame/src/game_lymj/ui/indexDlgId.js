2f0331060ae7ce5dced97fb2dd98893f78a93c2f
jinlong <jinlong@mq.com>
2017-07-20 09:00:16 +0800

    del more

--- CocosJSGame/src/game_lymj/ui/indexDlgId.js
+++
@@ -1,8 +0,0 @@
-
-
-var ID_DlgLymjMain = 20000;
-var ID_DlgLymjCardsInfo = 20001;
-var ID_DlgLymjResult = 20002;
-var ID_DlgLymjRankCenter = 20003;
-var ID_DlgLymjSetting = 20004;
-var ID_DlgLymjRule = 20005;
